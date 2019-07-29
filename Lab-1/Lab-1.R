11624846f0b6c7111afe7daa6971589f7548a50b
Brett <brettkim123@gmail,com>
2019-07-29 15:28:46 +1200

    finished Lab 1

--- Lab-1/Lab-1.R
+++ Lab-1/Lab-1.R
@@ -2,29 +2,33 @@ library("tidyverse")
 log_tib = read_csv("events_log.csv", col_types = 'cccccicii')
 log_tib = log_tib %>% separate(timestamp, c('Day', 'Time'), sep = 8)
 
+
 # Question 1a
- 
-click_through_df = 
-  log_tib %>% 
-  group_by(Day, session_id) %>% 
-  summarise(visited = sum(action == 'visitPage'), 
-            not_visited = sum(action != 'visitPage'), 
-            ratio = visited/not_visited) %>% 
-  group_by(Day) %>% 
-  summarise(visited = sum(visited), 
-            not_visited = sum(not_visited), 
-            ratio = visited/not_visited)
+test_df = data.frame(Day = c('Mon', 'Mon', 'Mon', 'Mon','Mon', 'Tue','Tue','Tue','Tue', 'Wed','Wed','Wed','Wed'), 
+                     session_id = c('a', 'x' ,'y','b','b','c','c','c','c','d','e','f','f'), 
+                     action = c('searchPage','checkin' ,'searchPage','searchPage','visitPage', 'searchPage', 'visitPage','searchPage','visitPage','searchPage', 'checkin', 'visitPage','visitPage'))
+
+test_df %>% group_by(Day, action) %>% summarise(n = n_distinct(session_id))
+
+test_df %>% group_by(Day) %>% summarise(n = n_distinct(session_id))
+test_df %>% group_by(Day,action) %>% summarise(n = n_distinct(session_id)) %>% filter(action == 'visitPage') %>% print()
+
+# the number of unique session_id's per day
+total_unique_sessions = log_tib %>% group_by(Day) %>% summarise(total_sessions = n_distinct(session_id))
+total_unique_sessions = total_unique_sessions[-1,] %>% print()
+
+# the number of session_id's that have 'visitpage' per day
+unique_sessions_visits = log_tib %>% group_by(Day,action) %>% summarise(visited = n_distinct(session_id)) %>% filter(action == 'visitPage') %>% print()
+
+answer_1a = unique_sessions_visits$visited/total_unique_sessions$total_sessions
 
 # Question 1b
-click_through_group_df = 
-  log_tib %>% 
-  group_by(Day, session_id,group) %>% 
-  summarise(visited = sum(action == 'visitPage'), 
-            not_visited = sum(action != 'visitPage'))%>% 
-  group_by(Day, group) %>%
-  summarise(visited = sum(visited), 
-            not_visited = sum(not_visited), 
-            ratio = visited/not_visited)
+total_unique_sessions_groups = log_tib %>% group_by(Day,group) %>% summarise(total_sessions = n_distinct(session_id))
+total_unique_sessions_groups = total_unique_sessions[-1,]
+unique_sessions_visits_groups = log_tib %>% group_by(Day,group, action) %>% summarise(visited = n_distinct(session_id)) %>% filter(action == 'visitPage') %>% print()
+
+answer_1b = unique_sessions_visits_groups$visited / total_unique_sessions_groups$total_sessions
+
 
 # Question 2
 
@@ -36,4 +40,5 @@ first_page_visit_df =
   group_by(session_id) %>% 
   summarise(first_page_position = first(result_position)) %>% 
   group_by(first_page_position) %>% 
-  summarise(count = n())
+  summarise(count = n()) %>%
+  mutate(proportion = count / sum(count))
